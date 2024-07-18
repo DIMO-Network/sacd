cd815a4f0d224ccb3864bfe62db9f865eca6044d
Lorran Sutter <sutterlorran@gmail.com>
2024-07-18 17:07:21 -0400

    State testing

--- test/SacdFactory.test.ts
+++ test/SacdFactory.test.ts
@@ -3,44 +3,80 @@ import { expect } from 'chai'
 import hre from 'hardhat'
 import { getAddress, parseGwei, isAddress } from 'viem'
 
+import * as C from './constants'
+
 describe.only('SacdFactory', function () {
   async function deployMockERC721() {
+    const [, user1] = await hre.viem.getWalletClients()
     const mockErc721 = await hre.viem.deployContract('MockERC721')
+    await mockErc721.write.mint([user1.account.address])
+
     return { mockErc721 }
   }
 
-  async function deploy() {
+  async function deploySacdFactory() {
+    const DEFAULT_EXPIRATION = BigInt((await time.latest()) + time.duration.years(1))
     const [owner, user1] = await hre.viem.getWalletClients()
 
     const sacdTemplate = await hre.viem.deployContract('Sacd')
     const sacdFactory = await hre.viem.deployContract('SacdFactory', [sacdTemplate.address])
 
-    return { sacdTemplate, sacdFactory, owner, user1 }
+    return { sacdTemplate, sacdFactory, owner, user1, DEFAULT_EXPIRATION }
   }
 
   describe('constructor', () => {
     it('Should correctly set the SACD template', async () => {
-      const { sacdTemplate, sacdFactory } = await loadFixture(deploy)
+      const { sacdTemplate, sacdFactory } = await loadFixture(deploySacdFactory)
 
       expect(await sacdFactory.read.sacdTemplate()).to.equal(getAddress(sacdTemplate.address))
     })
   })
 
   describe('createSacd', () => {
-    it('Test', async () => {
-      const { mockErc721 } = await loadFixture(deployMockERC721)
-      const { sacdFactory, owner, user1 } = await loadFixture(deploy)
+    context('State', () => {
+      it('Should create a new SACD with correct params', async () => {
+        const { mockErc721 } = await loadFixture(deployMockERC721)
+        const { sacdFactory, user1, DEFAULT_EXPIRATION } = await loadFixture(deploySacdFactory)
 
-      await sacdFactory.write.createSacd([mockErc721.address, 1n, 123n, owner.account.address, 1234n, ''], {
-        account: user1.account,
+        await sacdFactory.write.createSacd(
+          [mockErc721.address, 1n, C.MOCK_PERMISSIONS, user1.account.address, DEFAULT_EXPIRATION, ''],
+          {
+            account: user1.account,
+          }
+        )
+        const sacdCreatedEvents = await sacdFactory.getEvents.SacdCreated()
+
+        const sacdCreated = await hre.viem.getContractAt('Sacd', sacdCreatedEvents[0].args.sacdAddress as `0x${string}`)
+
+        expect(await sacdCreated.read.initialized()).to.be.true
+        expect(await sacdCreated.read.nftAddr()).to.equal(getAddress(mockErc721.address))
+        expect(await sacdCreated.read.tokenId()).to.equal(1n)
+        expect(await sacdCreated.read.permissions()).to.equal(C.MOCK_PERMISSIONS)
+        expect(await sacdCreated.read.grantee()).to.equal(getAddress(user1.account.address))
+        expect(await sacdCreated.read.expiration()).to.equal(DEFAULT_EXPIRATION)
+        expect(await sacdCreated.read.source()).to.equal('')
       })
+    })
 
-      const sacdCreatedEvents = await sacdFactory.getEvents.SacdCreated()
-      expect(sacdCreatedEvents).to.have.lengthOf(1)
-      expect(sacdCreatedEvents[0].args.nftAddress).to.equal(getAddress(mockErc721.address))
-      expect(sacdCreatedEvents[0].args.permissions).to.equal(123n)
-      expect(isAddress(sacdCreatedEvents[0].args.sacdAddress as string)).to.be.true
-      expect(sacdCreatedEvents[0].args.tokenId).to.equal(1n)
+    context('Events', () => {
+      it('Should emit SacdCreated with correct params', async () => {
+        const { mockErc721 } = await loadFixture(deployMockERC721)
+        const { sacdFactory, user1, DEFAULT_EXPIRATION } = await loadFixture(deploySacdFactory)
+
+        await sacdFactory.write.createSacd(
+          [mockErc721.address, 1n, C.MOCK_PERMISSIONS, user1.account.address, DEFAULT_EXPIRATION, ''],
+          {
+            account: user1.account,
+          }
+        )
+
+        const sacdCreatedEvents = await sacdFactory.getEvents.SacdCreated()
+        expect(sacdCreatedEvents).to.have.lengthOf(1)
+        expect(sacdCreatedEvents[0].args.nftAddress).to.equal(getAddress(mockErc721.address))
+        expect(sacdCreatedEvents[0].args.permissions).to.equal(C.MOCK_PERMISSIONS)
+        expect(isAddress(sacdCreatedEvents[0].args.sacdAddress as string)).to.be.true
+        expect(sacdCreatedEvents[0].args.tokenId).to.equal(1n)
+      })
     })
   })
 })
---
+++ test/constants.ts
@@ -0,0 +1,2 @@
+export const MOCK_PERMISSIONS = 816n // 11 00 11 00 00
+export const EXPIRATION_DEFAULT = parseInt(+new Date() / 1000)
